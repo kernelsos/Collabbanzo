c54e201d4cdbb1a5041be5609a61799845f2fc7a
Gagan Sharma <gagansharma800t@gmail.com>
2026-01-16 18:23:47 +0530

    First Commit

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,33 @@
+import { prismaClient } from "@/app/lib/db";
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
+        })
+    ],
+    callbacks: {
+        async signIn(params) {
+            console.log(params)
+            try {
+                await prismaClient.user.create({
+                    data: {
+                        email: "",
+                        provider: "Google"
+                    }
+
+                })
+            } catch (error) {
+                
+            }
+            return true;
+
+        }
+    }
+  
+})
+
+export { handler as GET, handler as POST }
---
+++ app/components/Appbar.tsx
@@ -0,0 +1,19 @@
+"use client";
+import {signIn, useSession, signOut} from "next-auth/react";
+
+
+export function Appbar() {
+    const session = useSession();
+    return <div>
+        <div className="flex justify-between">
+            <div>
+                Muze
+            </div>
+            <div>
+                {session.data?.user && <button className="m-2 p-2 bg-blue-400" onClick={() => signOut()}>LogOut</button> }
+                {!session.data?.user && <button className="m-2 p-2 bg-blue-400" onClick={() => signIn()}>SignIn</button> }
+                
+            </div>
+        </div>
+    </div>
+}
