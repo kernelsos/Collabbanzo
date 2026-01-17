78c0c5a84a4a3aa4351a00ceb936f125eecf5474
Gagan Sharma <gagansharma800t@gmail.com>
2026-01-17 20:25:55 +0530

    Added Streams Routes

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,12 +1,10 @@
 import { prismaClient } from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import {z} from "zod";
+//@ts-ignore
+import youtubesearchapi from "youtube-search-api";
 
 var YT_REGEX = /^(?:(?:https?:)?\/\/)?(?:www\.)?(?:m\.)?(?:youtu(?:be)?\.com\/(?:v\/|embed\/|watch(?:\/|\?v=))|youtu\.be\/)((?:\w|-){11})(?:\S+)?$/;
-
-
-
-
 
 const CreateStreamSchema = z.object({
     creatorId: z.string(),
@@ -27,14 +25,22 @@ export async function POST(req: NextRequest) {
 
         const extractedId = data.url.split("?v=")[1];
 
+        const res = await youtubesearchapi.GetVideoDetails(extractedId);
+        const thumbnails = res.thumbnail.thumbnails;
+        thumbnails.sort((a: {width:number},b:{width:number}) => a.width < b.width ? -1 : 1 );
+
+
         const stream = await prismaClient.stream.create({
             data: {
                 userId: data.creatorId,
                 url: data.url,
                 extractedId,
-                type: "Youtube"
-            }
-            
+                type: "Youtube",
+                title: res.title?? "Can't find video title",
+                smallImg: (thumbnails.length > 1? thumbnails[thumbnails.length - 2].url : thumbnails[thumbnails.length - 1].url) ?? "https://www.google.com/imgres?q=cute%20wallpaper&imgurl=https%3A%2F%2Fwww.shutterstock.com%2Fimage-photo%2Fbeautiful-wonderful-cute-kitten-wallpaper-600nw-2435308283.jpg&imgrefurl=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fcute-kitten-wallpaper&docid=lHLemrI0MqTLeM&tbnid=9ysvDOIhWHpjfM&vet=12ahUKEwjcqsfY5pKSAxX_SmwGHTFuHuAQM3oECG0QAA..i&w=528&h=600&hcb=2&ved=2ahUKEwjcqsfY5pKSAxX_SmwGHTFuHuAQM3oECG0QAA",
+                bigImg: thumbnails[thumbnails.length - 1].url ?? "https://www.google.com/imgres?q=cute%20wallpaper&imgurl=https%3A%2F%2Fwww.shutterstock.com%2Fimage-photo%2Fbeautiful-wonderful-cute-kitten-wallpaper-600nw-2435308283.jpg&imgrefurl=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fcute-kitten-wallpaper&docid=lHLemrI0MqTLeM&tbnid=9ysvDOIhWHpjfM&vet=12ahUKEwjcqsfY5pKSAxX_SmwGHTFuHuAQM3oECG0QAA..i&w=528&h=600&hcb=2&ved=2ahUKEwjcqsfY5pKSAxX_SmwGHTFuHuAQM3oECG0QAA"
+
+            }  
         })
 
         return NextResponse.json({
@@ -43,12 +49,25 @@ export async function POST(req: NextRequest) {
         })
 
     } catch(e) {
+        console.log(e);
         return NextResponse.json({
             message: "Error while adding a stream"
         },{
             status: 411
         })
     }
-    
+}
 
+export async function GET(req: NextRequest) {
+    const creatorId = req.nextUrl.searchParams.get("creatorId");
+    const streams = await prismaClient.stream.findMany({
+        where: {
+            userId: creatorId ?? ""
+
+        }
+    })
+
+    return NextResponse.json({
+        streams
+    })
 }
